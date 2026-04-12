d0f09165a6e968e16825bb8afd0610462f697206
Mgmdov <magomadov1195volk@gmail.com>
2026-04-13 01:50:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,29 @@
-const CACHE_NAME = 'muslim-tracker-v2';  // версию тоже обновил
+const CACHE_NAME = 'muslim-tracker-v1';
 const urlsToCache = [
-  './',                    // относительный путь
-  './index.html',
-  './manifest.json'
+  '/mgmdov.github.io/',
+  '/mgmdov.github.io/index.html',
+  '/mgmdov.github.io/manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  console.log('SW установлен');
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
-  self.skipWaiting();      // сразу активируем новый SW
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
-  );
-  self.clients.claim();    // сразу берём под контроль все вкладки
+  console.log('SW активирован');
+  event.waitUntil(clients.claim());
 });
