f591b3adacdbe9bc5e572ab18c1216845acdff2a
Mgmdov <magomadov1195volk@gmail.com>
2026-04-13 06:53:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,20 @@
 const CACHE_NAME = 'muslim-tracker-v1';
 const urlsToCache = [
-  '/mgmdov.github.io/',
-  '/mgmdov.github.io/index.html',
-  '/mgmdov.github.io/manifest.json'
+  '/',
+  '/index.html',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  console.log('SW установлен');
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
-});
-
-self.addEventListener('activate', event => {
-  console.log('SW активирован');
-  event.waitUntil(clients.claim());
 });
